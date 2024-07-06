e17ab1f48a2341ac9197597a095aaece7565060b
NagasakiS0y0 <3273105098@qq.com>
2024-07-06 20:10:45 +0800

    added song.h

--- song.h
+++ song.h
@@ -1,7 +1,13 @@
 #ifndef SONG_H
 #define SONG_H
 
-// 只声明结构体类型，不定义成员
-typedef struct song song;
+typedef struct song
+{
+	int count;  // 播放次数
+	char name[50];  // 歌曲名
+	char singer[50];  // 歌手
+	char address[1024];  // 歌曲绝对位置
+	struct song *next;  // 指向下一个节点
+} Song;
 
 #endif // SONG_H
